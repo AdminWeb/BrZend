920d82d6ad17e33da0732db98277339abf6bf96d
IgorDePaula <principe.borodin@gmail.com>
2015-04-10 10:32:41 -0300

    Improve quality

--- src/BrZend/View/Helper/CNPJ.php
+++ src/BrZend/View/Helper/CNPJ.php
@@ -9,20 +9,15 @@ class CNPJ extends AbstractHelper {
 
     public function __invoke($cnpj) {
         $len = strlen($cnpj);
-        $format = $len == 14? '0' . substr($cnpj, 0, 2) . '.' 
+        $format = substr($cnpj, 0, 2) . '.' 
                 . substr($cnpj, 2, 3) . '.' 
                 . substr($cnpj, 5, 3) . '/' 
                 . substr($cnpj, 8, 4) 
                 . '-' 
-                . substr($cnpj, 12, 2)
-                :
-            substr($cnpj, 0, 2) . '.' 
-                . substr($cnpj, 2, 3) . '.' 
-                . substr($cnpj, 5, 3) . '/' 
-                . substr($cnpj, 8, 4) 
-                . '-' 
-                . substr($cnpj, 12, 2)
-            ;        
+                . substr($cnpj, 12, 2);
+        if($len == 14){
+            $format = '0'.$format;
+        }
         return $format;
     }
 
