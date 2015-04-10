37f66588a2165738810ac0d0b9d4cada7d533f8a
IgorDePaula <principe.borodin@gmail.com>
2015-04-10 09:59:12 -0300

    Improve quality

--- src/BrZend/View/Helper/CNPJ.php
+++ src/BrZend/View/Helper/CNPJ.php
@@ -9,18 +9,20 @@ class CNPJ extends AbstractHelper {
 
     public function __invoke($cnpj) {
         $len = strlen($cnpj);
-        $format = '';
-        switch ($len) {
-            case 14:
-                $format = '0' . substr($cnpj, 0, 2) . '.' . substr($cnpj, 2, 3) . '.' . substr($cnpj, 5, 3) . '/' . substr($cnpj, 8, 4) . '-' . substr($cnpj, 12, 2);
-                break;
-            case 15:
-                $format = substr($cnpj, 0, 3) . '.' . substr($cnpj, 3, 3) . '.' . substr($cnpj, 6, 3) . '/' . substr($cnpj, 9, 4) . '-' . substr($cnpj, 13, 2);
-                break;
-            default:
-                throw new LengthException("O comprimento do argumento precisa estar entre 14 e 15 números/digitos!");
-                break;
-        }
+        $format = $len == 14? '0' . substr($cnpj, 0, 2) . '.' 
+                . substr($cnpj, 2, 3) . '.' 
+                . substr($cnpj, 5, 3) . '/' 
+                . substr($cnpj, 8, 4) 
+                . '-' 
+                . substr($cnpj, 12, 2)
+                :
+            substr($cnpj, 0, 2) . '.' 
+                . substr($cnpj, 2, 3) . '.' 
+                . substr($cnpj, 5, 3) . '/' 
+                . substr($cnpj, 8, 4) 
+                . '-' 
+                . substr($cnpj, 12, 2)
+            ;        
         return $format;
     }
 
