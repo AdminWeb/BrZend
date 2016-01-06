e893d02af9ec59b090ff16cfd66a7db06b064f0b
Igor C. de Paula <principe.borodin@gmail.com>
2016-01-06 17:13:19 -0200

    Removed violations

--- src/BrZend/View/Helper/CPF.php
+++ src/BrZend/View/Helper/CPF.php
@@ -1,28 +1,31 @@
 <?php
+
 /**
  * BrZend\View\Helper
- * 
- * @authorn
- * @version 
+ * @author
+ * @version
  */
+
 namespace BrZend\View\Helper;
 
 use Zend\View\Helper\AbstractHelper;
 use \InvalidArgumentException;
 use \LengthException;
+
 /**
  * View Helper
  */
+
 class CPF extends AbstractHelper
 {
     public function __invoke($cpf)
     {
-        if(!is_int($cpf)){
+        if (!is_int($cpf)) {
             throw new InvalidArgumentException("O argumento precisa ser do tipo inteiro!");
         }
-        if(strlen($cpf) != 11){
+        if (strlen($cpf) != 11) {
             throw new LengthException("O comprimento do argumento precisa ser de 11 numeros/digitos!");
-        }       
+        }
         return substr($cpf,0,3).'.'.substr($cpf,3,3).'.'.substr($cpf,6,3).'-'.substr($cpf,9,2);                
     }
 }
